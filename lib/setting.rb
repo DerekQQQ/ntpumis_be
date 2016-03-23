ce72aff0aee3345347cf93816e4c16c46bbbdedb
rainchiu <welly81211@gmail.com>
2016-03-24 02:06:35 +0800

    合聘教師順序更動

--- lib/setting.rb
+++ lib/setting.rb
@@ -2,6 +2,6 @@ class Setting
 
   TEACHER_TITLE_ARRAY_C = ['教授','副教授','助理教授','助教']
   TEACHER_TITLE_ARRAY_E = ['Professor','Associate Professor','Assisting Professor','Assistant']
-  TEACHER_EMPLOY_TYPE = ['專任教師','兼任教師','行政人員','他系教師','客座教授']
+  TEACHER_EMPLOY_TYPE = ['專任教師','合聘教師','兼任教師','他系教師','行政人員','客座教授']
 
 end
